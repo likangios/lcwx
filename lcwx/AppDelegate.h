242a22b92bc160a917224a8ace68f6d84cfceb27
luck <likangios@163.com>
2018-01-07 11:45:55 +0800

    Initial Commit

---
+++ lcwx/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  lcwx
+//
+//  Created by luck on 2018/1/7.
+//  Copyright © 2018年 luck. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ lcwx/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  lcwx
+//
+//  Created by luck on 2018/1/7.
+//  Copyright © 2018年 luck. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
