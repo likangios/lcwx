2337d6d252cf2ebd441c248c8e252a815b88be26
likangios <likangios@163.com>
2018-01-08 10:14:59 +0800

    no message

--- lcwx/Classes/Controller/Main/MainViewController.m
+++ lcwx/Classes/Controller/Main/MainViewController.m
@@ -21,21 +21,15 @@
 - (void)loadViewControllers{
     NSArray *vcs = @[@"BookShelfViewController",@"ChoicenessViewController",@"RankViewController",@"AccountViewController"];
     NSArray *taritemtitle = @[@"书架",@"精选",@"榜单",@"账户"];
-    NSArray *tabimg = @[@"tabbar_bokcase_22x21_",@"tab_jinx_24x21_",@"tab_ph_22x21_",@"tab_user_22x22_"];
-    NSArray *tabselimg = @[@"tabbar_bokcase_sel_22x21_",@"tab_jinx_sel_24x21_",@"tab_ph_sel_22x21_",@"tab_user_sel_22x22_"];
+    NSArray *tabimg = @[@"tabbar_bokcase",@"tab_jinx_24x21_",@"tab_ph_22x21_",@"tab_user_22x22_"];
+    NSArray *tabselimg = @[@"tabbar_bokcase_sel",@"tab_jinx_sel_24x21_",@"tab_ph_sel_22x21_",@"tab_user_sel_22x22_"];
     NSMutableArray *viewcontrollers = [NSMutableArray array];
     NSInteger count = 0;
     for (NSString *class_name in vcs) {
         UIViewController *vc = [[NSClassFromString(class_name) alloc]init];
-//        vc.title = navtitles[count];
         UITabBarItem *item =[[UITabBarItem alloc]initWithTitle:taritemtitle[count] image:[[UIImage imageNamed:tabimg[count]] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal] selectedImage:[[UIImage imageNamed:tabselimg[count]] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal]];
-
-        [item setTitleTextAttributes:@{NSFontAttributeName:[UIColor redColor]} forState:UIControlStateSelected];
+        [item setTitleTextAttributes:@{NSForegroundColorAttributeName:[UIColor colorWithHexString:@"f0554e"]} forState:UIControlStateSelected];
         vc.tabBarItem = item;
-        
-//        vc.tabBarItem.title = taritemtitle[count];
-//        vc.tabBarItem.image = [[UIImage imageNamed:tabimg[count]] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
-//        vc.tabBarItem.selectedImage =[[UIImage imageNamed:tabselimg[count]] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
         BaseNavigationController *nav = [[BaseNavigationController alloc]initWithRootViewController:vc];
         [viewcontrollers addObject:nav];
         count++;
